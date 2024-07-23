fe767e7e6c2530be15405e04d767e2fcb679d9c1
Chuanbao Lu <chuanbao.lu@gmail.com>
2024-07-23 20:53:16 +0000

    working copy

--- src/hover-box.js
+++ src/hover-box.js
@@ -1,10 +1,10 @@
-const template = document.createElement('template');
+const template = document.createElement("template");
 template.innerHTML = `
   <style>
     .box {
       position: absolute;
       height: auto;
-      border: solid 2px var(--border-color);
+      border: solid 2px;
       border-radius: 10px;
       margin-top: 0px;
       padding: 20px;
@@ -19,21 +19,19 @@ template.innerHTML = `
 
   <div class='hover-box'>
     <div id='hover-trigger'>
-      <slot name="hover-trigger"/>
+      <slot name='hover-trigger'/>
     </div>
-    <div id='hover-content' class='box' style="display:none">
-      <slot name="hover-content" />
+    <div id='hover-content' class='box' style='display:none'>
+      <slot name='hover-content' />
     </div>
   </div>
 `;
 
-
 const OFFSET = 6;
 
 customElements.define(
-  'hover-box',
+  "hover-box",
   class HoverBox extends HTMLElement {
-
     constructor() {
       super();
       this.getHiddenElementHeight = this.getHiddenElementHeight.bind(this);
@@ -41,66 +39,77 @@ customElements.define(
 
       this.timerHandle = undefined;
 
-      this.attachShadow({ mode: 'open' });
+      this.attachShadow({ mode: "open" });
 
       this.shadowRoot.appendChild(template.content.cloneNode(true));
-      this.hoverTrigger = this.shadowRoot.querySelector('#hover-trigger');
-      this.hoverContent = this.shadowRoot.querySelector('#hover-content');
+      this.hoverTrigger = this.shadowRoot.querySelector("#hover-trigger");
+      this.hoverContent = this.shadowRoot.querySelector("#hover-content");
 
-      this.width = parseInt(this.getAttribute('width') ?? '500');
-      this.color = this.getAttribute('color') ?? 'balck';
-      this.backgroundColor = this.getAttribute('backgroundColor') ?? 'white';
-      this.borderColor = this.getAttribute('borderColor') ?? '334155';
+      this.width = parseInt(this.getAttribute("width") ?? "500");
+      this.color = this.getAttribute("color") ?? "balck";
+      this.backgroundColor = this.getAttribute("backgroundColor") ?? "white";
+      this.borderColor = this.getAttribute("borderColor") ?? "334155";
 
-      this.hoverContent.style.color = this.color,
-      this.hoverContent.style.background = this.backgroundColor,
-      this.hoverContent.style.borderColor = this.borderColor
+      Object.assign(this.hoverContent.style, {
+        color: this.color,
+        background: this.backgroundColor,
+        borderColor: this.borderColor,
+        width: `${this.width}px`,
+      });
     }
 
     getHiddenElementHeight(ele) {
-      ele.style.left ='-99999px';
+      Object.assign(ele.style, {
+        left: "-99999px",
+        display: "block",
+      });
       const rect = ele.getBoundingClientRect();
-      ele.style.left = '0px';
-      ele.style.display = 'none';
+      Object.assign(ele.style, {
+        left: "0px",
+        display: "none",
+      });
       return rect;
     }
 
     onHover() {
-      //const {height: contentHeight, width: contentWidth} = this.getHiddenElementHeight(this.hoverContent);
-      const contentHeight = 500;
-      const contentWidht = 300;
-      
+      const { height: contentHeight, width: contentWidth } =
+        this.getHiddenElementHeight(this.hoverContent);
+
       const rect = this.hoverTrigger.getBoundingClientRect();
       const triggerTop = rect.top; // + window.scrollY;
       const triggerLeft = rect.left; //  + window.scrollX;
       const triggerBottom = rect.bottom; // + window.scrollY;
       const triggerRight = rect.right; // + window.scrollX;
-      const triggerHeight = rect.height;
-      const triggerWidth = rect.width;
-
-      window.innerHeight
-      window.innerWidth
 
       let contentTop = 0;
       let contentLeft = 0;
-      if (triggerBottom + OFFSET + contentHeight < window.innerHeight) {
+      if (triggerBottom + OFFSET + contentHeight <= window.innerHeight) {
         contentTop = triggerBottom + OFFSET;
       } else {
-        contentTop = triggerTop - OFFSET - contentHeight;
+        contentTop =
+          triggerTop - OFFSET - contentHeight < 0
+            ? triggerBottom + OFFSET
+            : triggerTop - OFFSET - contentHeight;
+      }
+      if (triggerRight + contentWidth < window.innerWidth) {
+        contentLeft = triggerLeft;
+      } else {
+        contentLeft =
+          triggerRight - contentWidth <= 0 ? 0 : triggerRight - contentWidth;
       }
 
-      this.hoverContent.style.left = `$(contentLeft)px`,
-      this.hoverContent.style.top = `$(contentTop)px`,
-      this.hoverContent.style.display = 'block';
+      Object.assign(this.hoverContent.style, {
+        left: `${contentLeft}px`,
+        top: `${contentTop + window.scrollY}px`,
+        display: "block",
+      });
     }
 
-    
-
     connectedCallback() {
-      this.hoverTrigger.addEventListener('mouseover', () => {
-           this.timerHandle = window.setTimeout(this.onHover, 500);
+      this.hoverTrigger.addEventListener("mouseover", () => {
+        this.timerHandle = window.setTimeout(this.onHover, 500);
       });
-      this.hoverTrigger.addEventListener('mouseout', (e) => {
+      this.hoverTrigger.addEventListener("mouseout", (e) => {
         if (this.timerHandle) {
           window.clearTimeout(this.timerHandle);
         }
@@ -113,7 +122,9 @@ customElements.define(
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-      console.log(`Attribute ${name} has changed. oldValue=${oldValue}, newValue=${newValue}`);
+      console.log(
+        `Attribute ${name} has changed. oldValue=${oldValue}, newValue=${newValue}`
+      );
     }
   }
 );
