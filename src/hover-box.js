1e0caa356590e83ff3bfdd3c3b9d73508e016b28
Chuanbao Lu <chuanbao.lu@gmail.com>
2024-07-25 19:29:52 +0000

    change to use floating close button

--- src/hover-box.js
+++ src/hover-box.js
@@ -5,16 +5,69 @@ template.innerHTML = `
     #hover-content {
       position: absolute;
       height: auto;
-      border: solid 2px;
+      border: solid 1px;
       border-radius: 0px;
-      margin-top: 0px;
+      margin-top: 0px 20px 0px 20px;
       padding: 0px;
       z-index: 999999;
+      background: white;
+      
     } 
+
+    .content-wrapper {
+      display: grid;
+      grid-auto-flow: row;
+      grid-template-columns: repeat(1, minmax(0, 1fr));
+      max-height: 250px; 
+      overflow-y: auto;
+    }
     
     #hover-trigger {
       width: max-content;
       height: max-content;
+    }
+    
+    .sr-only {
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      padding: 0;
+      margin: -1px;
+      overflow: hidden;
+      clip: rect(0, 0, 0, 0);
+      white-space: nowrap;
+      border-width: 0;
+    }
+    
+    #close-button {
+      color: black;
+      background-color: white;
+      border: solid 1px;
+      position: absolute;
+      top: -17px;
+      right: -17px;
+      border-radius: 50%;
+      padding: 0;
+      cursor: pointer
+    }
+
+    #close-button:hover:after {
+      content: ""; 
+      position: absolute; 
+      inset: -.225em; 
+      background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(255,255,255,1) 100%);
+      filter: blur(3px); 
+      border-radius: 50%; 
+      z-index: -1; 
+    }
+        
+    #close-button:focus {
+      --tw-ring-offset-width: 2px;
+      --tw-ring-opacity: 1;
+      --tw-ring-color: rgb(55 65 81 / var(--tw-ring-opacity))  
+      --tw-ring-offset-shadow: var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color);
+      --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+      box-shadow: var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000);
     }
   }
   </style>
@@ -23,9 +76,36 @@ template.innerHTML = `
     <div id='hover-trigger'>
       <slot name='hover-trigger'/>
     </div>
-    <div id='hover-content' style='display:none'>
-      <slot name='hover-content' />
+      <div id='hover-content' style='display:none'>
+        <button
+          type="button"
+          id="close-button"
+        >
+          <span class="sr-only">Close</span>
+          <!-- Close Icon -->
+          <svg
+            style="height:1.5rem; width:1.5rem"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
+        </button>
+        
+        <div class="content-wrapper">
+          <slot name='hover-content' />
+        </div>
+      </div>
     </div>
+    
   </div>
 `;
 
@@ -37,24 +117,21 @@ customElements.define(
     constructor() {
       super();
       this.getHiddenElementHeight = this.getHiddenElementHeight.bind(this);
-      this.onHover = this.onHover.bind(this);
-
-      this.timerHandle = undefined;
+      this.showHoverBox = this.showHoverBox.bind(this);
 
       this.attachShadow({ mode: "open" });
 
       this.shadowRoot.appendChild(template.content.cloneNode(true));
       this.hoverTrigger = this.shadowRoot.querySelector("#hover-trigger");
       this.hoverContent = this.shadowRoot.querySelector("#hover-content");
+      this.closeButton = this.shadowRoot.querySelector("#close-button");
 
       this.width = parseInt(this.getAttribute("width") ?? "500");
-      this.color = this.getAttribute("color") ?? "balck";
-      this.backgroundColor = this.getAttribute("backgroundColor") ?? "white";
-      this.borderColor = this.getAttribute("borderColor") ?? "334155";
+      this.borderColor = this.getAttribute("borderColor") ?? "#334155";
+
+      this.closeButton.style.borderColor = this.borderColor;
 
       Object.assign(this.hoverContent.style, {
-        color: this.color,
-        background: this.backgroundColor,
         borderColor: this.borderColor,
         width: `${this.width}px`,
       });
@@ -73,15 +150,15 @@ customElements.define(
       return rect;
     }
 
-    onHover() {
+    showHoverBox() {
       const { height: contentHeight, width: contentWidth } =
         this.getHiddenElementHeight(this.hoverContent);
 
       const rect = this.hoverTrigger.getBoundingClientRect();
-      const triggerTop = rect.top; // + window.scrollY;
-      const triggerLeft = rect.left; //  + window.scrollX;
-      const triggerBottom = rect.bottom; // + window.scrollY;
-      const triggerRight = rect.right; // + window.scrollX;
+      const triggerTop = rect.top;
+      const triggerLeft = rect.left;
+      const triggerBottom = rect.bottom;
+      const triggerRight = rect.right;
 
       let contentTop = 0;
       let contentLeft = 0;
@@ -108,25 +185,15 @@ customElements.define(
     }
 
     connectedCallback() {
-      this.hoverTrigger.addEventListener("mouseover", () => {
-        this.timerHandle = window.setTimeout(this.onHover, 500);
-      });
-      this.hoverTrigger.addEventListener("mouseout", (e) => {
-        if (this.timerHandle) {
-          window.clearTimeout(this.timerHandle);
-        }
+      this.hoverTrigger.addEventListener("click", this.showHoverBox);
+      this.closeButton.addEventListener("click", () => {
         this.hoverContent.style.display = "none";
       });
     }
 
     disconnectedCallback() {
       this.hoverTrigger.removeEventListener();
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-      console.log(
-        `Attribute ${name} has changed. oldValue=${oldValue}, newValue=${newValue}`
-      );
+      this.closeButton.removeEventListener();
     }
   }
 );
